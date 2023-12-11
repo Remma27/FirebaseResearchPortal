e198d69cc2bf1d9f53ce714d3dc84ce477accea8
Remma27 <emmanuelrsolano27@gmail.com>
2023-12-11 10:30:19 -0600

    Barra de progreso

--- public/js/investigation/addInvestigation.js
+++ public/js/investigation/addInvestigation.js
@@ -25,6 +25,10 @@ btnSaveProject.addEventListener('click', function (event) {
         return;
     }
 
+    // Show the progress container
+    const progressContainer = document.getElementById('progress-container');
+    progressContainer.style.display = 'block';
+
     // Get selected PDF file and image files
     const pdfFile = txtPdfFile.files[0];
     const imageFiles = txtImages.files;
@@ -40,66 +44,99 @@ btnSaveProject.addEventListener('click', function (event) {
 
         // Upload PDF file to storage and get its download URL
         const pdfUpload = container.child('pdfs/' + pdfFile.name).put(pdfFile, pdfMetadata);
-        pdfUpload.then(pdfSnapshot => pdfSnapshot.ref.getDownloadURL()).then(pdfUrl => {
 
-            // Promises for uploading image files and getting their download URLs
-            const imagePromises = Array.from(imageFiles).map((imageFile, index) => {
-                const imageMetadata = {
-                    contentType: imageFile.type
-                };
+        // Update progress bar during PDF upload
+        pdfUpload.on('state_changed',
+            function progress(snapshot) {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                const progressBar = document.getElementById('progress-bar');
+                const progressText = document.getElementById('progress-text');
+                progressBar.value = progress;
+                progressText.textContent = `Uploading PDF... ${Math.round(progress)}%`;
+            },
+            function error(error) {
+                alert('Error uploading PDF: ' + error.message);
+            },
+            function complete() {
+                // Promises for uploading image files and getting their download URLs
+                const imagePromises = Array.from(imageFiles).map((imageFile, index) => {
+                    const imageMetadata = {
+                        contentType: imageFile.type
+                    };
 
-                const imageUpload = container.child(`images/${index}_${imageFile.name}`).put(imageFile, imageMetadata);
-                return imageUpload.then(imageSnapshot => imageSnapshot.ref.getDownloadURL());
-            });
+                    const imageUpload = container.child(`images/${index}_${imageFile.name}`).put(imageFile, imageMetadata);
 
-            // Resolve all image upload promises
-            Promise.all(imagePromises)
-                .then(imageUrls => {
-                    // Check if the specified studentID exists in the 'students' collection
-                    const studentID = txtStudentID.value;
-                    db.collection("students").where("studentID", "==", studentID).get()
-                        .then((querySnapshot) => {
-                            if (!querySnapshot.empty) {
-                                // If student exists, generate a unique projectID
-                                const projectID = db.collection("researchProjects").doc().id;
+                    // Update progress bar during image upload
+                    imageUpload.on('state_changed',
+                        function progress(snapshot) {
+                            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                            const progressBar = document.getElementById('progress-bar');
+                            const progressText = document.getElementById('progress-text');
+                            progressBar.value = progress;
+                            progressText.textContent = `Uploading Images... ${Math.round(progress)}%`;
+                        },
+                        function error(error) {
+                            alert('Error uploading images: ' + error.message);
+                        }
+                    );
 
-                                // Save project details to Firestore
-                                db.collection("researchProjects").doc(projectID).set({
-                                    "projectID": projectID,
-                                    "researchTitle": txtResearchTitle.value,
-                                    "areaOfInterest": txtAreaofInterest.value,
-                                    "studentID": studentID,
-                                    "schoolGrade": txtSchoolGrade.value,
-                                    "topicDescription": txtTopicDescription.value,
-                                    "pdfUrl": pdfUrl,
-                                    "images": imageUrls,
-                                    "conclusions": txtConclusions.value,
-                                    "finalRecommendations": txtRecommendations.value,
-                                }).then(function () {
-                                    alert("Project Added Successfully: " + projectID);
-                                    clearFormFields(); // Clear form fields
-                                }).catch(function (error) {
-                                    alert("Error adding the document to Firestore: " + error.message);
-                                });
-                            } else {
-                                alert("The student with the specified studentID does not exist.");
-                            }
-                        })
-                        .catch((error) => {
-                            alert("Error checking if student exists: " + error.message);
-                        });
-
-                })
-                .catch(error => {
-                    alert('Error uploading images: ' + error.message);
+                    return imageUpload.then(imageSnapshot => imageSnapshot.ref.getDownloadURL());
                 });
 
-        })
-            .catch(pdfError => {
-                alert('Error uploading PDF: ' + pdfError.message);
-            });
+                // Resolve all image upload promises
+                Promise.all(imagePromises)
+                    .then(imageUrls => {
+                        // Check if the specified studentID exists in the 'students' collection
+                        const studentID = txtStudentID.value;
+                        db.collection("students").where("studentID", "==", studentID).get()
+                            .then((querySnapshot) => {
+                                if (!querySnapshot.empty) {
+                                    // If student exists, generate a unique projectID
+                                    const projectID = db.collection("researchProjects").doc().id;
+
+                                    // Get the download URL of the PDF file
+                                    pdfUpload.snapshot.ref.getDownloadURL()
+                                        .then(pdfUrl => {
+                                            // Save project details to Firestore
+                                            db.collection("researchProjects").doc(projectID).set({
+                                                "projectID": projectID,
+                                                "researchTitle": txtResearchTitle.value,
+                                                "areaOfInterest": txtAreaofInterest.value,
+                                                "studentID": studentID,
+                                                "schoolGrade": txtSchoolGrade.value,
+                                                "topicDescription": txtTopicDescription.value,
+                                                "pdfUrl": pdfUrl,
+                                                "images": imageUrls,
+                                                "conclusions": txtConclusions.value,
+                                                "finalRecommendations": txtRecommendations.value,
+                                            }).then(function () {
+                                                alert("Project Added Successfully: " + projectID);
+                                                clearFormFields(); // Clear form fields
+                                            }).catch(function (error) {
+                                                alert("Error adding the document to Firestore: " + error.message);
+                                            });
+                                        })
+                                        .catch(pdfError => {
+                                            alert('Error getting PDF download URL: ' + pdfError.message);
+                                        });
+                                } else {
+                                    alert("The student with the specified studentID does not exist.");
+                                }
+                            })
+                            .catch((error) => {
+                                alert("Error checking if student exists: " + error.message);
+                            });
+                    })
+                    .catch(error => {
+                        alert('Error uploading images: ' + error.message);
+                    });
+            }
+        );
     }
 });
+
+// Rest of the code...
+
 
 // Function to clear form fields
 function clearFormFields() {
@@ -120,6 +157,10 @@ function clearFormFields() {
     }
 
     txtResearchTitle.focus(); // Set focus to the Research Title input
+
+    // Hide the progress container after completion
+    const progressContainer = document.getElementById('progress-container');
+    progressContainer.style.display = 'none';
 }
 
 // Function to validate form inputs
